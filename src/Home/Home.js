86a6b85691e4cc7b9eb100948bfe01b27b2de45c
Greg Roques <greg.roques@gmail.com>
2020-04-14 18:29:06 -0400

    update

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -17,7 +17,14 @@ class Home extends Component {
                     Beds 4 Less specializes in selling top quality bedding from major bedding manufacturers at a fraction of the retail cost. Our mattresses and box springs are overstocks, close-outs, freight returns, and mismatched products. 
                 </div>
                 <iframe className={cssHome.homeVid} src="https://www.youtube.com/embed/eLzLlBRnOts?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+                <div className={cssHome.LAproud}>
+                    <div className={cssHome.LaHeader}>
+                        Louisiana Proud
+                    </div>
+                    <div className={cssHome.homeSubHeader}>
+                        Beds 4 Less is a locally owned and operated business. We are proud to serve the New Orleans Metropolitan Area. 
+                    </div>
+                </div>
             </div>
         )
     }
